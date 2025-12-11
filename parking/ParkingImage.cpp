c95e1f81fe0d20b1858c943546c35c548320c477
Alasdair Craig <ac@acraig.za.net>
2025-12-11 07:33:25 +0200

    Remove duplication, combined file output and bytedata output

--- parking/ParkingImage.cpp
+++ parking/ParkingImage.cpp
@@ -14,7 +14,7 @@ constexpr double ARROW_STROKE = 2.5;
 constexpr double PATH_STROKE = 3.0;
 
 
-bool MakeSVG(const std::string &filename, const CImageData &imageData)
+bool MakeSVG(const CImageData &imageData, const std::string &filename, std::string &byteData)
 {
 	svg::ShapeColl elements;
 
@@ -75,68 +75,9 @@ bool MakeSVG(const std::string &filename, const CImageData &imageData)
 	doc << background;
 	doc << elements;
 
-	if (doc.save())
-		return true;
-	return false;
-}
+	if (!filename.empty() && !doc.save())
+		return false;
 
-bool MakeSVG(std::string &byteData, const CImageData &imageData)
-{
-	svg::ShapeColl elements;
-
-	// Caps
-	for (const auto &polygon : imageData.polygons)
-		{
-		svg::Polygon poly{ svg::Fill{svg::Color{ 144, 238, 144 }}, svg::Stroke{POLY_STROKE, svg::Color::Green} };
-		for (const auto &pt : polygon.points)
-			{
-			poly << svg::Point{ pt.x, pt.y };
-			}
-		elements << poly;
-		}
-
-	// Bays
-	for (const auto &line : imageData.lines)
-		{
-		svg::Point p1(line.start.x, line.start.y);
-		svg::Point p2(line.end.x, line.end.y);
-		elements << svg::Line{ p1, p2, svg::Stroke{LINE_STROKE, svg::Color::Black} };
-		}
-
-	// Paths
-	for (const auto &line : imageData.paths)
-		{
-		svg::Point p1(line.start.x, line.start.y);
-		svg::Point p2(line.end.x, line.end.y);
-		elements << svg::Line{ p1, p2, svg::Stroke{PATH_STROKE, svg::Color::Blue} };
-		}
-
-	// Arrows
-	for (const auto &arrow : imageData.arrows)
-		{
-		svg::Polygon poly{ svg::Fill{svg::Color{ 238, 144, 144 }}, svg::Stroke{ARROW_STROKE, svg::Color::Red} };
-		for (const auto &pt : arrow.points)
-			{
-			poly << svg::Point{ pt.x, pt.y };
-			}
-		poly << svg::AnimateTransform{ svg::Point{ 0,0 }, svg::Point{ 10,10 }, "1s" };
-		elements << poly;
-		}
-
-	svg::Layout layout;
-	layout.dimensions = svg::Dimensions{ imageData.maximum.x - imageData.minimum.x, imageData.maximum.y - imageData.minimum.y };
-	layout.origin_offset = svg::Point{ -imageData.minimum.x, -imageData.minimum.y };
-	svg::Document doc("", layout);
-
-	svg::Polygon background(svg::Fill{ svg::Color{ 211, 211, 211 } });
-	background
-			<< svg::Point(0, 0)
-			<< svg::Point(layout.dimensions.width, 0)
-			<< svg::Point(layout.dimensions.width, layout.dimensions.height)
-			<< svg::Point(0, layout.dimensions.height);
-
-	doc << background;
-	doc << elements;
 	byteData = doc.toString();
 	return true;
 }
@@ -182,7 +123,9 @@ bool parking_layout_svg_file(const std::string &filename, const std::string &wkt
 	layout.Make(linePath, params);
 	CImageData imageData = layout.GetImageData(params);
 
-	return MakeSVG(filename, imageData);
+
+	std::string unused;
+	return MakeSVG(imageData, filename, unused);
 }
 
 bool parking_layout_svg_bytes(std::string &byteData, const std::string &wktGeometry, const std::string &jsonParams)
@@ -202,5 +145,6 @@ bool parking_layout_svg_bytes(std::string &byteData, const std::string &wktGeome
 	layout.Make(linePath, params);
 	CImageData imageData = layout.GetImageData(params);
 
-	return MakeSVG(byteData, imageData);
+	std::string unused;
+	return MakeSVG(imageData, unused, byteData);
 }
